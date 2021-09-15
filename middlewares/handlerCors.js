02d33c4c996963d47ac281ad419ee513d27116ac
Рустам <Rustamaaaa@bk.ru>
2021-09-15 23:27:25 +0300

    fix/Пробую исправить CORS

--- middlewares/handlerCors.js
+++ middlewares/handlerCors.js
@@ -6,19 +6,24 @@ const allowedCors = [
 // Обработка CORS
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  // const { method } = req;
+  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+  // const requestHeaders = req.headers['access-control-request-headers'];
 
   // res.header('Access-Control-Allow-Origin', origin);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept,Authorization,Origin');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // if (method === 'OPTIONS') {
+  //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  //   res.header('Access-Control-Allow-Headers', requestHeaders);
+  //   return res.end();
+  // }
   return next();
 };
