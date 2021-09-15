44dd4f46f916df90b73ac019a2fd108bb6687c84
Рустам <Rustamaaaa@bk.ru>
2021-09-15 23:28:40 +0300

    fix/Пробую исправить CORS

--- middlewares/handlerCors.js
+++ middlewares/handlerCors.js
@@ -1,22 +1,22 @@
-const allowedCors = [
-  'http://movies-diploma-api.nomoredomains.monster',
-  'https://movies-diploma-api.nomoredomains.monster',
-];
+// const allowedCors = [
+//   'http://movies-diploma-api.nomoredomains.monster',
+//   'https://movies-diploma-api.nomoredomains.monster',
+// ];
 
 // Обработка CORS
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   // const { method } = req;
   // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
   // const requestHeaders = req.headers['access-control-request-headers'];
 
   // res.header('Access-Control-Allow-Origin', origin);
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
 
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept,Authorization,Origin');
-  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
